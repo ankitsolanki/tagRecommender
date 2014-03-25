30a51de21d7358ff603f0f2c6ce6a0557ca53a1a
Ankit Solanki <ankit.solanki90@gmail.com>
2014-03-25 11:45:30 +0800

    Halo code tested

--- src/halo/src/halo.py
+++ src/halo/src/halo.py
@@ -2,6 +2,7 @@ import json
 import json,sys
 from SPARQLWrapper import SPARQLWrapper, JSON
 from SPARQLWrapper.Wrapper import jsonlayer
+from pymongo import MongoClient
 class halo:
 	def __init__(self):
 		self.config = json.load(open("../config/config.json"))
@@ -9,40 +10,48 @@ class halo:
 		self.termDB = MongoClient()["semantified"]["terms"]
 		self.halodb = MongoClient()["halo"]["halos"]
 	def run(self,query):
-		print(query)
-		self.sparql.setQuery(query)
-		self.sparql.setReturnFormat(JSON)
-		result = self.sparql.query()
-		#jsonlayer.use('cjson')
-		body = result.response.read().encode('ascii','ignore')
-		fixed_body = body.decode("ascii")
-		result = jsonlayer.decode(fixed_body)
-		return result["results"]["bindings"]
+		try:
+			self.sparql.setQuery(query)
+			self.sparql.setReturnFormat(JSON)
+			result = self.sparql.query()
+			#jsonlayer.use('cjson')
+			body = result.response.read().encode('ascii','ignore')
+			fixed_body = body.decode("ascii")
+			result = jsonlayer.decode(fixed_body)
+			return result["results"]["bindings"]
+		except :
+			print(query)
+			time.sleep(60)
+			return self.run(query)
 
 	def makeQuery(self,uri,querykey):
 		return  self.config[querykey] % (uri)
 	def insert(self,obj):
 		self.halodb.update({"_id":obj["_id"]},obj,True)
-
+	def isprocessed(self,uri):
+		return len(list(self.halodb.find({"_id" : uri}))) > 0
 	def getHalo(self,uri):
-		query = self.makeQuery(uri,"queryone")
-		result = self.run(query)
-		query = self.makeQuery(uri,"querytwo")
-		result.extend(self.run(query))
-		halo = {}
-		halo["_id"] = uri
-		halo["uri"] = uri
-		halo["halo"] = {}
-		for each in result:
-			halouri = each["aura"]["value"]
-			halo["halo"][halouri.replace(".","$")] = {}
-			obj = {}
-			obj["halouri"] = halouri
-			obj["count"] = each["count"]["value"]
-			obj["label"] = each["label"]["value"]
-			halo["halo"][halouri.replace(".","$")] = obj
-		self.insert(halo)
-
+		if not self.isprocessed(uri):
+			query = self.makeQuery(uri,"queryone")
+			result = self.run(query)
+			query = self.makeQuery(uri,"querytwo")
+			result.extend(self.run(query))
+			halo = {}
+			halo["_id"] = uri
+			halo["uri"] = uri
+			halo["halo"] = {}
+			for each in result:
+				halouri = each["aura"]["value"]
+				halo["halo"][halouri.replace(".","$")] = {}
+				obj = {}
+				obj["halouri"] = halouri
+				obj["count"] = each["auraCount"]["value"]
+				obj["label"] = each["label"]["value"]
+				halo["halo"][halouri.replace(".","$")] = obj
+			self.insert(halo)
+			print("processed halo for : " + uri)
+		else :
+			print("previously processed uri : " + uri )
 	def getdatadb(self):
 		return self.termDB.find()
 	
@@ -54,4 +63,5 @@ class halo:
 
 if __name__ == "__main__":	
 	h = halo()
-	h.getHalo("http://dbpedia.org/resource/Eclipse")
+	h.processhalofromdb()
+	#h.getHalo("http://dbpedia.org/resource/Eclipse")
